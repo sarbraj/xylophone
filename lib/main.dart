b4ceb354078074d1fd1e3a78b512c592e56c663d
Sarbraj Dulai <sarbrajdulai01@gmail.com>
2024-08-17 17:27:55 +1000

    Finished app that plays sounds.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+// import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  Future<void> playAudioHandler(int noteNumber) async {
+    final player = AudioPlayer();
+    await player.play(AssetSource('note$noteNumber.wav'));
+  }
+
+  Expanded buildKey(Color color, int audioFile) {
+    return Expanded(
+        child: Card(
+      color: color,
+      child: TextButton(
+        onPressed: () => {playAudioHandler(audioFile)},
+        child: Text(''),
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.blue, 5),
+              buildKey(Colors.indigoAccent, 6),
+              buildKey(Colors.purple, 7),
+            ],
+          ),
         ),
       ),
     );
